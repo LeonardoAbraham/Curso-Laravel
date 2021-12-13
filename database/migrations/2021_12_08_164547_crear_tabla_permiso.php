9c09f65d5f783d231382f21b74ad43b2f138f334
*leonardo* <*leon.alonzo.leo@gmail.com*>
2021-12-13 16:42:55 -0600

    Migraciones

--- database/migrations/2021_12_08_164547_crear_tabla_permiso.php
+++ database/migrations/2021_12_08_164547_crear_tabla_permiso.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrearTablaUsuario extends Migration
+class CrearTablaPermiso extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CrearTablaUsuario extends Migration
      */
     public function up()
     {
-        Schema::create('usuario', function (Blueprint $table) {
+        Schema::create('permiso', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('usuario',50);
-            $table->string('password',100);
             $table->string('nombre',50);
+            $table->string('slug',50);
             $table->timestamps();
             $table->charset = 'utf8mb4';
             $table->collation = 'utf8mb4_spanish_ci';
@@ -31,6 +30,6 @@ class CrearTablaUsuario extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('usuario');
+        Schema::dropIfExists('permiso');
     }
 }
